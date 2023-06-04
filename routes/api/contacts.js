2efb412742a0c9d03fcd325e0ada2370a37098e2
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-06-04 11:38:56 +0300

    CREATED: schema and model

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,14 +4,22 @@ const router = express.Router();
 
 const contactsControllers = require('../../controllers/contactsControllers');
 
+const { isValidId } = require('../../middlewares');
+
 router.get('/', contactsControllers.getAllContacts);
 
-router.get('/:contactId', contactsControllers.getContactsById);
+router.get('/:contactId', isValidId, contactsControllers.getContactsById);
 
 router.post('/', contactsControllers.addContact);
 
-router.delete('/:contactId', contactsControllers.deleteContactById);
+router.delete('/:contactId', isValidId, contactsControllers.deleteContactById);
 
-router.put('/:contactId', contactsControllers.updateContactById);
+router.put('/:contactId', isValidId, contactsControllers.updateContactById);
+
+router.patch(
+  '/:contactId/favorite',
+  isValidId,
+  contactsControllers.updateStatusContact
+);
 
 module.exports = router;
