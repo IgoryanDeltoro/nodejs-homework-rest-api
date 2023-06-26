e40e199ecf8de6934930576be08469b1d0d5dea3
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-06-26 20:06:51 +0300

    ADDED: sending verify to mail

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -2,9 +2,13 @@ const bcrypt = require('bcrypt');
 
 const gravatar = require('gravatar');
 
+const { nanoid } = require('nanoid');
+
 const { User } = require('../../models');
 
-const { HttpError } = require('../../helpers');
+const { HttpError, sendEmail } = require('../../helpers');
+
+const { BASE_URL } = process.env;
 
 const userRegister = async (req, res) => {
   const { email, password } = req.body;
@@ -18,11 +22,23 @@ const userRegister = async (req, res) => {
   const avatarURL = gravatar.url(email, { s: '250' }, false);
 
   const hashedPassword = await bcrypt.hash(password, 10);
+
+  const verificationToken = nanoid();
+
   const newUser = await User.create({
     ...req.body,
     avatarURL,
     password: hashedPassword,
+    verificationToken,
   });
+
+  const verifyEmail = {
+    to: email,
+    subject: 'Verify email',
+    html: `<a target="_blank" href="${BASE_URL}/api/users/verify/${verificationToken}">Click to verify email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
 
   return res.status(201).json({
     user: {
