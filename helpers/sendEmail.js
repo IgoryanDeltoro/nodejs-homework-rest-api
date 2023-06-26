6995c10d6be9cc15615775b6855ce420b7267bff
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-06-26 20:10:28 +0300

    CREATED: middleware for handing email verify

---
+++ helpers/sendEmail.js
@@ -0,0 +1,23 @@
+const nodemailer = require('nodemailer');
+
+const { META_PASSWORD, META_EMAIL } = process.env;
+
+const config = {
+  host: 'smtp.meta.ua',
+  port: 465,
+  secure: true,
+  auth: {
+    user: META_EMAIL,
+    pass: META_PASSWORD,
+  },
+};
+
+const transporter = nodemailer.createTransport(config);
+
+const sendEmail = async data => {
+  const email = { ...data, from: META_EMAIL };
+  await transporter.sendMail(email);
+  return true;
+};
+
+module.exports = sendEmail;
